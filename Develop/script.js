7dee6b5a54a9f4d35bc8a0f298661b44b2cf74b6
trswork <87744214+trswork@users.noreply.github.com>
2021-08-22 13:44:02 -0400

    final update

--- Develop/script.js
+++ Develop/script.js
@@ -1,121 +1,107 @@
 // Assignment code here
-const passwordEl = document.getElementById('password');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numberEl = document.getElementById('number');
-const symbolEl = document.getElementById('symbol');
-const generateEl = document.getElementById('generate');
 
-const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomsymbol
-};
+var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alpha = "abcdefghijklmnopqrstuvwxyz";
+var Num = "0123456789";
+var Sym = "!@#$%^&*=-_";
 
-
-  function getRandomLower(){
-    var confirmLowerCase = 'abcdefghijklmnopqrstuvwxyz';
-    return confirmLowerCase[Math.floor(Math.random() * lower.length)];
-}
-function getRandomUpper(){
-    var confirmUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    return confirmUpperCase[Math.floor(Math.random() * upper.length)];
-}
-function getRandomNumber(){
-    var confirmNumber = '0123456789';
-    return confirmNumber[Math.floor(Math.random() * number.length)]; 
-}
-function getRandomsymbol() {
-    var confirmSymbol = '!@#$%^&*=-_';
-    return symbol[Math.floor(Math.random() * symbols.length)];
-}
- console.log()
-   
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var length;
+var caps;
+var alpha;
+var special;
+var numeric;
+
 
 // Write password to the #password input
-function writePassword(length, upperCase, lowerCase, symbol, number) {
-  var password = generatePassword();
-  for(var i = 0; i < length; i++){
-    password = generatePassword(Math.floor(Math.random() * Password.Length));
-
+function writePassword() {
+  
+    var password = generatePassword(''); 
+    function generatePassword(length) {
+        var alpha = "abcdefghijklmnopqrstuvwxyz";
+        var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var numeric = "0123456789";
+        var special = "!@#$%^&*=-_";
+    
+        var options = [alpha, caps, numeric, special];
+    
+        var password = "";
+        var passwordArray = Array(length);
+    
+        for (i = 0; i < length; i++) {
+            var currentOption = options[Math.floor(Math.random() * options.length)];
+            var randomChar = currentOption.charAt(Math.floor(Math.random() * currentOption.length));
+            password += randomChar;
+            passwordArray.push(randomChar);
+        }
+    
+        checkPassword();
+    
+        function checkPassword() {
+            var missingValueArray = [];
+            var containsAll = true;
+    
+            options.forEach(function (e, i, a) {
+                var hasValue = false;
+                passwordArray.forEach(function (e1, i1, a1) {
+                    if (e.indexOf(e1) > -1) {
+                        hasValue = true;
+                    }
+                });
+    
+                if (!hasValue) {
+                    missingValueArray = a;
+                    containsAll = false;
+                }
+            });
+    
+            if (!containsAll) {
+                passwordArray[Math.floor(Math.random() * passwordArray.length)] = missingValueArray.charAt(Math.floor(Math.random() * missingValueArray.length));
+                password = "";
+                passwordArray.forEach(function (e, i, a) {
+                    password += e;
+                });
+                checkPassword();
+            }
+        }
+    
+    return password;
+}
+    
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-    
-}}
- 
+  passwordText.innerText = password;
+
+}
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", () => {
-    length= + lengthEl.value;
-    haslower= lowercaseEl;
-    hasupper= uppercaseEl;
-    hasnumber= numberEl;
-    hassymbol= symbolEl;
-
-    passwordEl = generatePassword(
-        haslower,
-        hasupper,
-        hasnumber,
-        hassymbol,
-        length
-    );
-});
-
-function generatePassword(lower, upper, number, symbol, length) {
-
-    const typesCount = lower + upper + number + symbol;
-
-    console.log('typesCount: ',typesCount);
-
-    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
-    (item => Object.values(item)[0]);
-
-    console.log('typesArr ', typesArr);
-
-    for(let i = 0; i < length; i += typesCount) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            console.log('funcName: ',funcName);
-
-            generatePassword += randomFunc[funcName] ();
-        })
-    }
+generateBtn.addEventListener("click", writePassword);
     
-    const password = generatePassword.slice(0, length);
+var PasswordLength = prompt ("Password Length: select between 8 and 128");
+        console.log("Password Length" + PasswordLength);
 
-    return password;
-
-    };
-
-var length = window.prompt ("Password Length: select between 8 and 12");
-        console.log("Password Length" + length);
-
-        if(!length) {
+        if(!PasswordLength) {
             alert("Required Field");
-        } else if (length < 8  || length > 12){
-            length = window.prompt("Must have between 8 and 12");
+        } else if (PasswordLength < 8  || PasswordLength > 128){
+            PasswordLength = prompt("Must have between 8 and 128");
         }
        
-        var upperCase = window.confirm("Are you including uppercase letters?");
-        console.log("Upper Case " + upperCase);
+        var caps = confirm("Are you including uppercase letters?");
+        console.log("Upper Case " + caps);
 
-        var lowerCase = window.confirm("Are you including lowercase letters?");
-        console.log("Lower Case " + lowerCase);
+        var alpha = confirm("Are you including lowercase letters?");
+        console.log("Lower Case " + alpha);
 
-        var symbol = window.confirm("Are you including symbols?");
-        console.log("symbol " + symbol);
+        var special = confirm("Are you including symbols?");
+        console.log("symbol " + special);
 
-        var number = window.confirm("Are you including numbers?");
-        console.log("number " + number);
+        var numeric = confirm("Are you including numbers?");
+        console.log("number " + numeric);
 
             
-
-
         
 
